42bdd51528355b8796788cce3b06c59dc6086eee
NC03 <nc03developer@gmail.com>
2020-12-24 16:23:15 -0800

    rewriting python script

--- scene.py
+++ scene.py
@@ -2,67 +2,66 @@ import re
 import sys
 import os
 
+class Object:
+    def __init__(self,type,d):
+        self.type = type
+        self.d = d
+    def __str__(self):
+        return self.type+": "+str(self.d)
+    def isInstance(self,typeName):
+        return self.type.lower() == typeName.lower()
+    def format(self):
+        out = ""
+        out += self.type.upper()
+        if self.isInstance("camera"):
+            out += " "+self.d["p"].replace(","," ")
+            out += " "+self.d["u"].replace(","," ")
+            out += " "+self.d["v"].replace(","," ")
+            out += " "+self.d["f"]
+        elif self.isInstance("sphere"):
+            out += " "+self.d["p"].replace(","," ")
+            out += " "+self.d["r"]
+        elif self.isInstance("checkeredsphere"):
+            out += " "+self.d["p"].replace(","," ")
+            out += " "+self.d["r"]
+            out += " "+self.d["n"].replace(","," ")
+            out += " "+self.d["s"].replace(","," ")
+            out += " "+self.d["c1"].replace(","," ")
+            out += " "+self.d["c2"].replace(","," ")
+        elif self.isInstance("plane"):
+            out += " "+self.d["p"].replace(","," ")
+            out += " "+self.d["u"].replace(","," ")
+            out += " "+self.d["v"].replace(","," ")
+            out += " "+self.d["c"].replace(","," ")
+        elif self.isInstance("checkeredplane"):
+            out += " "+self.d["p"].replace(","," ")
+            out += " "+self.d["u"].replace(","," ")
+            out += " "+self.d["v"].replace(","," ")
+            out += " "+self.d["c1"].replace(","," ")
+            out += " "+self.d["c2"].replace(","," ")
+            out += " "+self.d["w"]
+            out += " "+self.d["h"]
+        elif self.isInstance("triangle"):
+            out += " "+self.d["v1"].replace(","," ")
+            out += " "+self.d["v2"].replace(","," ")
+            out += " "+self.d["v3"].replace(","," ")
+            out += " "+self.d["c"].replace(","," ")
+        return out
 
-def process(inputFile, out):
-    elements = [(line.split(":")[0], line.split(":")[1].strip())
-                for line in inputFile if ":" in line and (line.strip())[0] != "#"]
-    for type, arguments in elements:
-        out.write(type.upper()+" ")
-        if type.lower() == "camera":
-            pattern = re.compile(
-                r"p\((.*)\).*u\((.*)\).*v\((.*)\).*f\((.*)\)")
-            m = pattern.match(arguments)
-            out.write(" ".join(m.group(1).split(","))+" ")
-            out.write(" ".join(m.group(2).split(","))+" ")
-            out.write(" ".join(m.group(3).split(","))+" ")
-            out.write(m.group(4)+" ")
-        elif type.lower() == "sphere":
-            pattern = re.compile(r"p\((.*)\).*r\((.*)\)")
-            m = pattern.match(arguments)
-            out.write(" ".join(m.group(1).split(","))+" ")
-            out.write(m.group(2)+" ")
-        elif type.lower() == "plane":
-            pattern = re.compile(
-                r"p\((.*)\).*u\((.*)\).*v\((.*)\).*c\((.*)\)")
-            m = pattern.match(arguments)
-            out.write(" ".join(m.group(1).split(","))+" ")
-            out.write(" ".join(m.group(2).split(","))+" ")
-            out.write(" ".join(m.group(3).split(","))+" ")
-            out.write(" ".join(m.group(4).split(","))+" ")
-        elif type.lower() == "checkeredplane":
-            pattern = re.compile(
-                r"p\((.*)\).*u\((.*)\).*v\((.*)\).*c1\((.*)\).*c2\((.*?)\).*w\((.*?)\).*h\((.*?)\)")
-            m = pattern.match(arguments)
-            out.write(" ".join(m.group(1).split(","))+" ")
-            out.write(" ".join(m.group(2).split(","))+" ")
-            out.write(" ".join(m.group(3).split(","))+" ")
-            out.write(" ".join(m.group(4).split(","))+" ")
-            out.write(" ".join(m.group(5).split(","))+" ")
-            out.write(m.group(6)+" ")
-            out.write(m.group(7)+" ")
-        elif type.lower() == "checkeredsphere":
-            pattern = re.compile(
-                r"p\((.*)\).*r\((.*)\).*n\((.*)\).*s\((.*)\).*c1\((.*)\).*c2\((.*?)\)")
-            m = pattern.match(arguments)
-            out.write(" ".join(m.group(1).split(","))+" ")
-            out.write(m.group(2)+" ")
-            out.write(" ".join(m.group(3).split(","))+" ")
-            out.write(" ".join(m.group(4).split(","))+" ")
-            out.write(" ".join(m.group(5).split(","))+" ")
-            out.write(" ".join(m.group(6).split(","))+" ")
-        elif type.lower() == "triangle":
-            pattern = re.compile(
-                r"v1\((.*)\).*v2\((.*)\).*v3\((.*)\).*c\((.*)\)")
-            m = pattern.match(arguments)
-            out.write(" ".join(m.group(1).split(","))+" ")
-            out.write(" ".join(m.group(2).split(","))+" ")
-            out.write(" ".join(m.group(3).split(","))+" ")
-            out.write(" ".join(m.group(4).split(","))+" ")
+def getArguments(line):
+    type, args = line.split(":")
+    type = type.strip().lower()
+    pattern = re.compile(r"([_\w\d]+)\((.+?)\)")
+    return Object(type, dict(pattern.findall(args)))
 
+def process(inputFile, outputFile):
+    objects = [getArguments(l) for l in inputFile if ":" in l and l.strip()[0] != "#"]
+    for o in objects:
+        outputFile.write(o.format()+"\n")
 
 def main(args):
     if len(args) <= 2:
-        print("Usage: python3 scene.py input_file.scene")
+        print("Usage: python3 scene.py input_file.scene output_file.ppm")
     else:
         inputName = sys.argv[1]
         outputName = sys.argv[2]
@@ -71,8 +70,8 @@ def main(args):
         process(f, out)
         f.close()
         out.close()
-        os.system(
-            "./raytracing {} {}".format(inputName+".stream", outputName))
+        cmd = "./raytracing {} {}".format(inputName+".stream", outputName)
+        os.system(cmd)
 
 
 if __name__ == "__main__":
